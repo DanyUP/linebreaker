155167c0012128702da854abb92502f06ac7670d
Daniele Armellini <dany@example.com>
2011-10-04 00:15:45 +0200

    Adding Javadoc to BreakAction.

--- src/main/java/com/danielearmellini/linebreaker/BreakAction.java
+++ src/main/java/com/danielearmellini/linebreaker/BreakAction.java
@@ -1,11 +1,31 @@
 package com.danielearmellini.linebreaker;
 
+/**
+ * Class that implements the action to be applied to the equivalence class computed by
+ * line breaker
+ * @author Daniele Armellini
+ *
+ * @param <T>
+ */
 public interface BreakAction<T> {
 
+	/**
+	 * Method executed on the first element of every equivalence class
+	 * @param firstElement
+	 */
 	public void doFirst(T firstElement);
 	
+	/**
+	 * Method executed on every element of the equivalence class (including the first
+	 * and the last element)
+	 * @param element
+	 */
 	public void doCurrent(T element);
 	
+	/**
+	 * Method executed on the last element of every equivalence class
+	 * @param lastElement
+	 */
 	public void doLast(T lastElement);
 	
 }
